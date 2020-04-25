dfa8b7580068337cba7fa2d8b29a34d7f1bd97d5
ravid7 <rrathor2@stevens.edu>
2020-04-25 17:16:10 -0400

    initial 2

---
+++ CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/rathore/Garbage/CMake
+# Build directory: /home/rathore/Garbage/CMake
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(Test1 "/home/rathore/Garbage/CMake/First")
+set_tests_properties(Test1 PROPERTIES  _BACKTRACE_TRIPLES "/home/rathore/Garbage/CMake/CMakeLists.txt;12;add_test;/home/rathore/Garbage/CMake/CMakeLists.txt;0;")
---
+++ Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]){
+    if(argc > 1)
+        return 1;
+    cout<<"..."<<endl;
+    return 0;
+}
